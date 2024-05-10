599a15f9d07628bc8dcf427415628b5a7fd71783
JamieKessler <jkessler5535@gmail.com>
2024-05-10 16:57:47 -0700

    changed the .date to dislay block

--- weather-app/src/Pages/Home.js
+++ weather-app/src/Pages/Home.js
@@ -8,15 +8,18 @@ const Home = () => {
   <div className="container">
     <div>
       <div className="app">
-         <div className="display-weather">
-            <div className="weather-app-h1">
-              <h1>Weather App</h1>
-            </div>
-            <div className="location">
-              <div className="city-temp">
-                <h2 className="temperature">29&#176;</h2>
-                <h2 className="city">Dhaka</h2>
-                <p classame="location-p">9:30am - Friday, 27 May '22</p>
+          <div className="display-weather">
+              <div className="weather-app-h1">
+                <h1>Weather App</h1>
+              </div>
+              <div className="location">
+                <div className="city-temp">
+                  <h2 className="temperature">29&#176;</h2>
+                  <h2 className="city">Dhaka</h2>
+                </div>
+                  <div className="location-p">
+                    <p>9:30am - Friday, 27 May '22</p>
+                  </div>
               </div>
          
               <div className="calendar">
@@ -28,33 +31,33 @@ const Home = () => {
                 <div className="box">June 02</div>
                 <div className="box">June 03</div>
               </div>
-            </div>
-        </div>
-
-        <div className="details-container">
-          <div className="weather-details">
-            <h2>Weather Details</h2>
-            <div></div>
-            <div className="grid-container">
-              <div className="left-column">
-                <ul>
-                  <li>Feels like</li>
-                  <li>Cloudy</li>
-                  <li>Humidity</li>
-                  <li>Wind</li>
-                </ul>
-              </div>
-              <div className="right-column">
-                <ul>
-                  <li>37&#176;</li>
-                  <li>86%</li>
-                  <li>12pm</li>
-                  <li>62%</li>
-                  <li>8km/h</li>
-                </ul>
-              </div>
-            </div>
           </div>
+      
+
+          <div className="details-container">
+            <div className="weather-details">
+              <h2>Weather Details</h2>
+              <div></div>
+              <div className="grid-container">
+                <div className="left-column">
+                  <ul>
+                    <li>Feels like</li>
+                    <li>Cloudy</li>
+                    <li>Humidity</li>
+                    <li>Wind</li>
+                  </ul>
+                </div>
+                <div className="right-column">
+                  <ul>
+                    <li>37&#176;</li>
+                    <li>86%</li>
+                    <li>12pm</li>
+                    <li>62%</li>
+                    <li>8km/h</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
 
           <div className="hours-container">
             <div>
