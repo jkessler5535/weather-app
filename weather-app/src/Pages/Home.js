2acd4d9104ad5b6cfa199f25e048a9956f0ad986
JamieKessler <jkessler5535@gmail.com>
2024-05-11 11:45:12 -0700

    added margin/padding to li items. changed opacity on black overlay. chamged px on .underline

--- weather-app/src/Pages/Home.js
+++ weather-app/src/Pages/Home.js
@@ -12,15 +12,19 @@ const Home = () => {
               <div className="weather-app-h1">
                 <h1>Weather App</h1>
               </div>
-              <div className="location">
-                <div className="city-temp">
-                  <h2 className="temperature">29&#176;</h2>
-                  <h2 className="city">Dhaka</h2>
-                </div>
-                  <div className="location-p">
-                    <p>9:30am - Friday, 27 May '22</p>
+            <div className="location">
+              <div className="city-temp">
+                  <div className="temperature">
+                    <h2>29&#176;</h2>
                   </div>
+                <div className="city">
+                  <h2>Dhaka</h2>
+                   <p className="date">9:30am - Friday, 27 May '22</p>
+                   <p className="date date-right">Partly Cloudy</p>
+                </div>
               </div>
+            </div>
+              
          
               <div className="calendar">
                 <div className="box">May 28</div>
@@ -33,60 +37,61 @@ const Home = () => {
               </div>
           </div>
       
-
+          
           <div className="details-container">
-            <div className="weather-details">
-              <h2>Weather Details</h2>
-              <div></div>
-              <div className="grid-container">
-                <div className="left-column">
-                  <ul>
-                    <li>Feels like</li>
-                    <li>Cloudy</li>
-                    <li>Humidity</li>
-                    <li>Wind</li>
-                  </ul>
+              <div className="weather-details">
+                <div className="heading">
+                  <h2>Weather Details</h2>
                 </div>
-                <div className="right-column">
-                  <ul>
-                    <li>37&#176;</li>
-                    <li>86%</li>
-                    <li>12pm</li>
-                    <li>62%</li>
-                    <li>8km/h</li>
-                  </ul>
+                <div className="underline"></div>
+                <div className="grid-container">
+                  <div className="left-column">
+                    <ul>
+                      <li>Feels like</li>
+                      <li>Cloudy</li>
+                      <li>Humidity</li>
+                      <li>Wind</li>
+                    </ul>
+                  </div>
+                  <div className="right-column">
+                    <ul>
+                      <li>37&#176;</li>
+                      <li>86%</li>
+                      <li>62%</li>
+                      <li>8km/h</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
 
-          <div className="hours-container">
-            <div>
-              <h2>Next Hours</h2>
-            </div>
-            <div></div>
-            <div class="grid-container">
-              <div class="left-column-2">
-                <ul>
-                  <li>10am</li>
-                  <li>11am</li>
-                  <li>12pm</li>
-                  <li>1pm</li>
-                  <li>2pm</li>
-                </ul>
-              </div>
-              <div class="right-column-2">
-                <div className="right-column-2">
-                  <ul>
-                    <li>30&#176;</li>
-                    <li>31&#176;</li>
-                    <li>31&#176;</li>
-                    <li>31&#176;</li>
-                    <li>31&#176;</li>
-                  </ul>
+              <div className="hours-container">
+                <div className="heading">
+                  <h2>Next Hours</h2>
+                </div>
+                <div className="underline"></div>
+                <div class="grid-container">
+                  <div class="left-column">
+                    <ul>
+                      <li>10am</li>
+                      <li>11am</li>
+                      <li>12pm</li>
+                      <li>1pm</li>
+                      <li>2pm</li>
+                    </ul>
+                  </div>
+                  <div class="right-column-2">
+                    <div className="right-column">
+                      <ul>
+                        <li>30&#176;</li>
+                        <li>31&#176;</li>
+                        <li>31&#176;</li>
+                        <li>31&#176;</li>
+                        <li>31&#176;</li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
